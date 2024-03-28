14430347293307ca14df883a67d697c6c1d4be0a
vinissyn <vlogvinicius37@gmail.com>
2024-03-27 21:03:14 -0300

    adiciona entrada de usuario e imprime o resultado

---
+++ src/aula1/Main.java
@@ -0,0 +1,7 @@
+package aula1;
+import javax.swing.JOptionPane; 
+public class Main {
+	public static void main(String[] args) {
+		  JOptionPane.showMessageDialog(null, "Bem-Vindo a interface do sistema");
+	}
+}
